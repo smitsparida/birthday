11be99775f249195fb0853b9ac6fff8030040cf2
smitsparida <parida.smita10@gmail.com>
2026-02-01 22:30:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ var config = {
      * Add your girlfriend’s photo if you want
      */
     imgs: {
-        "My Love, My Happiness": "./imgs/girlfriend.png",
+        "My Love, My Happiness": "https://github.com/smitsparida/birthday/blob/43ad7f7fbbc9883416f9879c3b6edfbe8613897c/imgs/smp.jpeg",
         // You can add more images like:
         // "Today is a very special day": "./imgs/birthday.jpg",
     },
